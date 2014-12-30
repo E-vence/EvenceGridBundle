6d04ac6ef28e2d636b5998f826fca22cf4cef33a
Ruben Harms <postbus@rubenharms.nl>
2014-12-30 18:19:02 +0100

    Namespace change in service injection

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -3,7 +3,7 @@ use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Parameter;
 
-$container->setDefinition('evence.grid', new Definition('Evence\Bundle\CoreBundle\Grid\GridHelper', array(
+$container->setDefinition('evence.grid', new Definition('Evence\Bundle\GridBundle\Grid\GridHelper', array(
     new Reference('doctrine'),
     new Reference('templating'),
     new Reference('request_stack'),
